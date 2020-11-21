448148c1edec84493b3a999271333f684bec9d1f
Dat Huu Vo <dvo9@uwo.ca>
2020-11-21 15:59:25 -0500

    move

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,17 @@
+
+    
 dailyValue = 0
-usr = input("u gud?")
-if usr.lower() == 'yes' or 'y':
+usr = input("u gud? ")
+if usr.lower() == "yes" or 'y':
+    print("why")
     dailyValue = 1
-else:
+elif usr.lower() == "no" or 'n':
     dailyValue = 0
 
-print(dailyValue)
+if dailyValue == 1:
+    print("you entered yes")
+elif dailyValue == 0:
+    print("you entered no")
+
+usr = input("That’s great! Tell me about it! \n")
+print("your response: ",usr)
